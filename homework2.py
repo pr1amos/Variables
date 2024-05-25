76dd2187a3b136509ec545169d5eb8fd44aeaa3e
pr1amos <157016419+pr1amos@users.noreply.github.com>
2024-05-25 18:25:13 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+homework = 12
+spent_hours = 1.5
+
+course = 'Python'
+average_value = spent_hours / homework
+print(f'На курсе: {course}, было выполнено {homework} заданий, всего затрачено {spent_hours} часов, '
+      f'в среднем на одно задание тратится {average_value} часов')
